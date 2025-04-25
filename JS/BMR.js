04e1f9c650222a2680f87bfd319d4ece0b2e3c28
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-04-25 22:49:55 +0200

    "whey" to "protein" mistake changed

--- JS/BMR.js
+++ JS/BMR.js
@@ -8,20 +8,20 @@
     const BMRwithActivity = (66 + (13.7 * weight) + (5 * height) - (6.8 * age)) * activity;
     const finalCaloricRequirement = Math.round(BMRwithActivity);
 
-    const whey = Math.round(weight * 2);
+    const protein = Math.round(weight * 2);
     const fat = Math.round((finalCaloricRequirement * 0.3) / 9);
-    const carbs = Math.round((finalCaloricRequirement - (fat * 9) - (whey * 4)) / 4);
+    const carbs = Math.round((finalCaloricRequirement - (fat * 9) - (protein * 4)) / 4);
 
     document.querySelector(".js-BMRkcal").innerHTML = finalCaloricRequirement;
-    document.querySelector(".js-BMRwhey").innerHTML = whey;
+    document.querySelector(".js-BMRprotein").innerHTML = protein;
     document.querySelector(".js-BMRfat").innerHTML = fat;
     document.querySelector(".js-BMRcarbs").innerHTML = carbs;
     document.querySelector(".js-BMRmeals").innerHTML = mealsPerDay;
 
     document.querySelector(".js-plannedPerMealKcal").innerHTML =
         Math.round((finalCaloricRequirement / mealsPerDay) / 10) * 10;
-    document.querySelector(".js-plannedPerMealWhey").innerHTML =
-        Math.round((whey / mealsPerDay) / 5) * 5;
+    document.querySelector(".js-plannedPerMealProtein").innerHTML =
+        Math.round((protein / mealsPerDay) / 5) * 5;
     document.querySelector(".js-plannedPerMealFat").innerHTML =
         Math.round((fat / mealsPerDay) / 5) * 5;
     document.querySelector(".js-plannedPerMealCarbs").innerHTML =
